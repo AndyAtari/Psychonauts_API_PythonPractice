4563158ddbb5432bbdbe582188ce226632a0554e
AndyAtari <andrewsalteri@gmail.com>
2021-07-13 17:26:28 -0400

    Created characters and home templates. Added link that redirects to an api get request.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import requests
 import jsonify
+import requests
+
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 if __name__ == "__main__":
@@ -8,8 +10,15 @@ if __name__ == "__main__":
 
 @app.route("/")
 def home():
-    response = requests.get("https://psychonauts-api.herokuapp.com/api/characters?name=raz")
-    print(type(response))
-    return response.json()
+    return render_template("home.html")
 
-   
+@app.route("/raz")
+def characters(): 
+    response = requests.get("https://psychonauts-api.herokuapp.com/api/characters?name=raz")
+    data = response.json() 
+    name = data['name']
+    img = data['img']
+    powers = data['psiPowers']
+    for x in powers: 
+     psi = powers
+    return render_template("characters.html", raz = name, pic = img, psi_powers = psi)
